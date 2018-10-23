96e2e1dc2e69338c354c8387104dff5994a8622f
dogzday <dogzday94@gmail.com>
2018-10-23 05:24:34 -0700

    initial commit.

---
+++ src/view/Main.java
@@ -0,0 +1,34 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+
+        GridPane grid = new GridPane();
+        Label label = new Label("Hello world.");
+        Scene scene = new Scene(grid, 300, 275);
+        grid.add(label, 0, 0);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
