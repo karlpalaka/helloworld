a3794f0de49d8000113b398fc945e38d39d3c0c2
dogzday <dogzday94@gmail.com>
2018-10-23 05:30:05 -0700

    removed useless code and fixed strings for consistencies.

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,8 +1,6 @@
 package view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -14,8 +12,8 @@ public class Main extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello world.");
 
         GridPane grid = new GridPane();
         Label label = new Label("Hello world.");
