a775e12894ed891a76f771eb89890b204e25afa5
Jasleen Kaur <jazzk0@users.noreply.github.com>
2018-10-24 17:52:22 -0700

    added menu :)

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -9,7 +9,7 @@ public class Controller
         // init game with board, player, cards
         // player does one of three things
         // alternate players
-        Scanner input = new Scanner(System.in);
+
 
         Board board = new Board();
         Player jas = new Player("Jas", 'O');
@@ -19,7 +19,7 @@ public class Controller
         // this is our end condition when a player has <= 2 trains left,
         // at game init players have 45 each
         boolean playersHaveTrains = true;
-        int option;
+        boolean temp = false;
 
         // this is the main game loop
         while(playersHaveTrains)
@@ -29,32 +29,40 @@ public class Controller
             // gabe's turn third
             // we run each object's turn phase
             // here we have players play
-
-            System.out.println("1 for Draw Train Cards");
-            System.out.println("2 for Claim a Route");
-            System.out.println("3 for Draw Destination Tickets");
-            System.out.print("Choose an option: ");
-            int choice = input.nextInt();
-            boolean temp = false;
-            switch (choice)
-            {
-                case 1:
-                    temp = drawTrainCards(jas);
-                    break;
-                case 2:
-                    temp = claimRoute(jas);
-                    break;
-                case 3:
-                    temp  = drawDestinationTickets(jas);
-                    break;
-            }
-
+            temp = playerTurn(jas);
+            temp = playerTurn(rey);
+            temp = playerTurn(gabe);
+            
             // the SOLE end condition for the game
             if (rey.getTrainsRemaining() <= 2 || jas.getTrainsRemaining() <= 2 || gabe.getTrainsRemaining() <= 2) break;
         }
 
 
 
+    }
+
+    private static boolean playerTurn(Player player)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1 for Draw Train Cards");
+        System.out.println("2 for Claim a Route");
+        System.out.println("3 for Draw Destination Tickets");
+        System.out.print("Choose an option: ");
+
+        int choice = input.nextInt();
+
+        switch (choice)
+        {
+            case 1:
+                return drawTrainCards(player);
+            case 2:
+                return claimRoute(player);
+            case 3:
+                return drawDestinationTickets(player);
+        }
+
+        return false;
     }
 
     private static boolean drawTrainCards(Player player) {
