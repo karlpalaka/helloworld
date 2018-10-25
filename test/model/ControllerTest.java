2a8eb04b13983793f3fb46a7c1b9a4c1cb9287e3
Jasleen Kaur <jazzk0@users.noreply.github.com>
2018-10-24 22:54:39 -0700

    added controllerTest

---
+++ test/model/ControllerTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest
+{
+
+
+    @Test
+    public void drawTrainCardsTest()
+    {
+
+    }
+}
