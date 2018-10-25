ffcd70777b46a9b9d2a06cc22304687c52e96ffe
dogzday <dogzday94@gmail.com>
2018-10-24 23:28:09 -0700

    added infos, a couple methods, and more shuffling options

--- src/model/Cards.java
+++ src/model/Cards.java
@@ -5,23 +5,35 @@ import java.util.Random;
 
 public class Cards
 {
-    public static int CARD_LIMIT = 96;
+    public static final int CARD_LIMIT = 96;
+    public static final int SHUFFLE_TIMES = 1; // large values will slow game down
 
     // todo should be 30 for final game, but here are 9 destination cards
-    // todo also missing points per card
-    private char[][] destinationCards = {{0, 0, 2, 0}, // G
-                                         {0, 0, 0, 2}, // P
-                                         {0, 2, 0, 4}, // W
-                                         {0, 2, 2, 2}, // !
-                                         {2, 0, 4, 0}, // R
-                                         {2, 4, 4, 4}, // Y
-                                         {4, 0, 4, 2}, // B
-                                         {4, 2, 4, 2}, // O
-                                         {0, 4, 2, 4}}; // L (Blue)
+    /**
+     * A destination card is a pair of coordinates. <br>
+     * Consider it as (x1, y1) and (x2, y2). <br>
+     * It is used for specifying conditions for <br>
+     * to earn points. The last column specifies <br>
+     * score earned from completed destination.
+     * @return Destination cards.
+     */
+    private char[][] destinationCards = {{0, 0, 2, 0, 3}, // G
+                                         {0, 0, 0, 2, 3}, // P
+                                         {0, 2, 0, 4, 3}, // W
+                                         {0, 2, 2, 2, 1}, // ! - 1 point because easiest
+                                         {2, 0, 4, 0, 3}, // R
+                                         {2, 4, 4, 4, 3}, // Y
+                                         {4, 0, 4, 2, 3}, // B
+                                         {4, 2, 4, 2, 3}, // O
+                                         {0, 4, 2, 4, 3}}; // L (Blue)
 
     // todo should be 110 for final game, missing 14 wildcards
     private char[] trainCards;
 
+    /**
+     * Initializes, fills, and randomizes trainCards <br>
+     * SHUFFLE_TIMES number of times.
+     */
     public Cards()
     {
         trainCards = new char[CARD_LIMIT];
@@ -35,7 +47,10 @@ public class Cards
         Arrays.fill(trainCards, 72, 83, 'O');
         Arrays.fill(trainCards, 84, 95, 'L');
 
-        randomizeArray(trainCards);
+        for (int i = 0; i < SHUFFLE_TIMES; ++i)
+        {
+            randomizeArray(trainCards);
+        }
     }
 
     // ========== getters ==========
@@ -53,6 +68,29 @@ public class Cards
     }
 
     /**
+     * Returns a row of destinationCards. <br>
+     * A destination card is a pair of coordinates. <br>
+     * Consider it as (x1, y1) and (x2, y2). <br>
+     * It is used for specifying conditions for <br>
+     * to earn points.
+     * @param r Specifies row to return.
+     * @return Row specified by r.
+     */
+    public char[] getDesintationCard(int r)
+    {
+        if (r < 0 || r > 5) throw new ArrayIndexOutOfBoundsException();
+
+        char[] temp = new char[5];
+
+        for (int i = 0; i < 5; ++i)
+        {
+            temp[i] = destinationCards[r][i];
+        }
+
+        return temp;
+    }
+
+    /**
      * Used for player placement conditions. <br>
      * If player has enough of a certain color, <br>
      * they can place trains on the Board.
@@ -60,7 +98,7 @@ public class Cards
      */
     public char[] getTrainCards()
     {
-        return trainCards;
+        return this.trainCards;
     }
 
     // ========== setters ==========
@@ -75,6 +113,8 @@ public class Cards
     {
         this.trainCards = trainCards;
     }
+
+    // ========== helpers  ==========
 
     private static char[] randomizeArray(char[] array)
     {
