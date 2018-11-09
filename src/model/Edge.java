2a39fc57a9fa5293a4ab223a46f4226800084c31
COSLAB <COSLAB@A116879.ad.csueastbay.edu>
2018-11-09 12:32:20 -0800

    added things for jas to do.

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -5,11 +5,13 @@ public class Edge
     int source;
     int destination;
     int weight;
+    RouteColor routeColor;
 
-    public Edge(int source, int destination, int weight)
+    public Edge(int source, int destination, int weight, RouteColor routeColor)
     {
         this.source = source;
         this.destination = destination;
         this.weight = weight;
+        this.routeColor = routeColor;
     }
 }
