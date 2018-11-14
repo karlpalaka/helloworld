adc8d2b23d5203a21d66a59bff7a33b8eb2a5bed
COSLAB <COSLAB@134.154.250.141>
2018-11-14 12:55:59 -0800

    n/a

--- src/helloworlds/HelloWorlds.java
+++ src/helloworlds/HelloWorlds.java
@@ -5,6 +5,10 @@
  */
 package helloworlds;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 /**
  *
  * @author karpa
@@ -17,7 +21,34 @@ public class HelloWorlds
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        List<String> ls = new ArrayList<String>();
+        List<Integer> li = new ArrayList<Integer>();
+        
+        ls.add("cat");
+        ls.add("dog");
+        ls.add("bird");
+        ls.add("chicken");
+        
+        li.add(1);
+        li.add(2);
+        li.add(3);
+        li.add(4);
+        
+        Predicate<String> animal = i -> (i.equals("bird"));
+        Predicate<Integer> number = i -> (i == 4);
+        System.out.println((linearSearch(ls, animal)));
+        System.out.println((linearSearch(li, number)));
     }
     
+    public static <T> int linearSearch(List<T> list, Predicate<T> check)
+    {
+        for (T temp: list)
+        {  
+            if (check.test(temp))
+            {
+                return list.indexOf(temp);
+            }
+        }
+        return -1;
+    }
 }
