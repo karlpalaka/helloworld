e8c917275b91654ca295e2c17c313c61d1d88ccc
COSLAB <COSLAB@134.154.250.141>
2018-11-14 11:37:41 -0800

    n/a

--- HelloWorlds/src/helloworlds/HelloWorlds.java
+++ HelloWorlds/src/helloworlds/HelloWorlds.java
@@ -9,15 +9,13 @@ package helloworlds;
  *
  * @author COSLAB
  */
-public
-        class HelloWorlds
+public class HelloWorlds
 {
 
     /**
      * @param args the command line arguments
      */
-    public static
-            void main(String[] args)
+    public static void main(String[] args)
     {
         // TODO code application logic here
     }
